c9a7654d5de324d42249958c5498db1d38dac45a
sanjitk7 <sanjitk2018@gmail.com>
2023-05-08 15:48:22 +0530

    test for capture

--- Tests/SimpleNetworkAnalyzerTest/packetCaptureTest.cpp
+++ Tests/SimpleNetworkAnalyzerTest/packetCaptureTest.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
 #include "PcapLiveDevice.h"
 #include "../../packetCapture/packetCapture.h"
+#include <iostream>
+#include <string>
+#include <cstring>
+
+// using ::testing::HasSubstr;
 
 // Demonstrate some basic assertions.
 TEST(PacketCaptureTest, basicCaptureAssersions) {
@@ -8,9 +13,16 @@ TEST(PacketCaptureTest, basicCaptureAssersions) {
   std::string interfaceIPAddr = "192.168.1.14";
 
   PacketCapture packetCapture(interfaceIPAddr);
-  // packetCapture.doCaptureAndParse();
   pcpp::PcapLiveDevice *dev = packetCapture.openDevice();
+
+  std::string packetHeader = PacketMetaData::createHeader(*dev, 12030204932095034, 64);
   
-  EXPECT_EQ(dev->getIPv4Address().toString(), "192.168.1.14");
+  const char* found_sampleID = strstr(packetHeader.c_str(), "12030204932095034");
+  const char* found_sampleSize = strstr(packetHeader.c_str(), "64");
+  const char* found_recordType = strstr(packetHeader.c_str(), "H");
+
+  EXPECT_TRUE(found_sampleID);
+  EXPECT_TRUE(found_sampleSize);
+  EXPECT_TRUE(found_recordType);
   
 }
